8abce852ca6cd4aa76b933f5d3c046de095ae73d
Esteban Marini V <158347567+EstebanMaVi@users.noreply.github.com>
2024-03-20 16:56:59 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import FirstApp from './firstApp'
+
+const App = () =>{
+  return (
+    <>
+      <h1> Hola Mundo! </h1>
+      <h2> Bienvenido </h2>
+      <FirstApp/>
+    </>
+  )
+}
+
+export default App
---
+++ firstApp.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+const firstApp = () =>{
+  return (
+    <>
+    <span>
+      <h1> Tag </h1>
+      <p> 10 </p>
+    </span>
+    </>
+  )
+}
+
+export default firstApp
